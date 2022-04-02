67f629846c8dd2e234ad386c98e4109a45793c37
Sam Drage <samdrage@sams-mbp.d.port.ac.uk>
2022-04-02 12:49:54 +0100

    Added directory scanning

--- image-gen/generate.py
+++ image-gen/generate.py
@@ -1,15 +1,29 @@
 ### Create all combinations of image
-from PIL import image
+from PIL import Image
+from os import listdir, getcwd
+from os.path import isfile, join
+import shutil
 
-# Layer body outline
+## Get list of all fish
+mypath = getcwd()+'/components/'
+bodies = [f for f in listdir(mypath+'body/') if isfile(join(mypath+'body/', f))]
+eyes = [f for f in listdir(mypath+'eyes/') if isfile(join(mypath+'eyes/', f))]
+hats = [f for f in listdir(mypath+'hat/') if isfile(join(mypath+'hat/', f))]
+print(bodies)
+print(eyes)
+print(hats)
 
+## Make copy of body image
+#shutil.copyfile()
 
-# Get colour layer based on body layer
+## Layer body outline
 
-# Recolour colour layer
+## Get colour layer based on body layer
 
-# Layer on colour
+## Recolour colour layer
 
-# Add eyes
+## Layer on colour
 
-# Save image
+## Add eyes
+
+## Save image
