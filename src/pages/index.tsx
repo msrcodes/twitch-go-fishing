4b1941de74e611c2c4fd8168531e594d91f4c23c
Mikael Rozee <mikaelsrozee@gmail.com>
2022-04-02 12:55:45 +0100

    feat: render OBJ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import {useEffect} from 'react';
+
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader';
+import {Canvas, useLoader} from '@react-three/fiber';
+
 import Fish from '../components/Fish';
 import TwitchClient from '../helpers/twitch/client';
 
@@ -10,8 +15,16 @@ const Homepage = () => {
 
   const fishes = new Array(10).fill(0);
 
+  // TODO: this is hacky eks dee
+  const obj = useLoader(OBJLoader, 'http://localhost:3000/rod.obj');
+
   return (
     <main className="w-screen h-screen overflow-hidden">
+      <Canvas className="z-50">
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <primitive object={obj} />
+      </Canvas>
       {fishes.map(() => (
         <Fish size={64} />
       ))}
