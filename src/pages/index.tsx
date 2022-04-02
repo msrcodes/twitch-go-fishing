fa2766db8fc7940dc73053b53f64bbbb71f7e159
Mikael Rozee <mikaelsrozee@gmail.com>
2022-04-02 10:46:46 +0100

    feat: connect twitch chat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
-const Homepage = () => (
-  <main>
-    <h1 className="text-red-500">Hello World!</h1>
-  </main>
-);
+import {useEffect} from 'react';
+import TwitchClient from '../helpers/twitch/client';
+
+const Homepage = () => {
+  useEffect(() => {
+    const client = new TwitchClient();
+    client.init();
+  }, []);
+
+  return (
+    <main>
+      <h1 className="text-red-500">Hello World!</h1>
+    </main>
+  );
+};
 
 export default Homepage;
