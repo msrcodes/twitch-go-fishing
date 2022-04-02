b2ce29163a70279335e82a0a61decda1362b05be
Mikael Rozee <mikaelsrozee@gmail.com>
2022-04-02 15:16:20 +0100

    feat: add score

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import Fish from '../components/Fish';
 import Image from 'next/image';
 import Rod from '../components/Rod';
+import {useState} from 'react';
 
 const Homepage = () => {
-  const fishes = new Array(10).fill(0);
+  const fishes = new Array(20).fill(0);
+
+  const [score, setScore] = useState(0);
+
+  const addScore = () => {
+    setScore(score + 1);
+  };
 
   return (
     <main className="w-screen h-screen overflow-hidden">
+      <div className="z-40 absolute text-3xl top-8 left-8 font-extrabold text-white">
+        Fish Caught: {score}
+      </div>
+
       <Image
-        width={1920}
-        height={1080}
         objectFit="cover"
         src="/bg.png"
         alt="Fish"
@@ -20,7 +29,7 @@ const Homepage = () => {
         className="z-20"
       />
 
-      <Rod />
+      <Rod addScore={addScore} />
 
       {fishes.map((_fish, i) => (
         <Fish key={i} size={64} />
