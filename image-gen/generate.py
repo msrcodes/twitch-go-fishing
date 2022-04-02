ee0f397631d81a63a2f874c9cd78d03dc6729929
Sam Drage <samdrage@sams-mbp.d.port.ac.uk>
2022-04-02 11:50:08 +0100

    image gen python file created

---
+++ image-gen/generate.py
@@ -0,0 +1,15 @@
+### Create all combinations of image
+from PIL import image
+
+# Layer body outline
+
+
+# Get colour layer based on body layer
+
+# Recolour colour layer
+
+# Layer on colour
+
+# Add eyes
+
+# Save image
